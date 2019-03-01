4e1c87d86f46d96aad5f2faf087a12735750b77d
adelplanque <adelplanque@users.noreply.github.com>
2019-03-01 08:22:18 +0100

    update setup

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(
     description='Use autotools to configure python extension build',
     package=find_packages(),
     url='https://github.com/adelplanque/autotools-extension',
+    license='MIT',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: POSIX',
         'Topic :: Software Development :: Build Tools',
     ]
